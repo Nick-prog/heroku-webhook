7b9f26393b8f6b5d1ac076c53e9249c5352dd621
Nickolas Rodriguez <52770090+Nick-prog@users.noreply.github.com>
2022-01-13 14:50:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import Webhook
+import flask
+from flask import Flask, request, abort
 
-Webhook.uploaded_documents()
-Webhook.fee_documents()
+app = Flask(__name__)
+@app.route('/webhook', methods=['POST'])
+def get_webhook():
+    if request.method == 'POST':
+       Webhook.start()
+    else:
+        abort(400)
+        
+if __name__ == '__main__':
+    app.run()
