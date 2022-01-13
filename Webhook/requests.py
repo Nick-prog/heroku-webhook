fd4863fd0a516bfc26374a480b8503bd7cc917fb
Nickolas Rodriguez <52770090+Nick-prog@users.noreply.github.com>
2022-01-13 15:20:10 -0600

    Update requests.py

--- Webhook/requests.py
+++ Webhook/requests.py
@@ -2,11 +2,4 @@ import Webhook
 import json
 
 def start(data):
-    dict = data
-    list = dict["data"]
-
-    if(len(list) != 0):
-        for total in range (len(list)):
-            if(list[total].get("event").get("method") == "POST" and list[total].get("event").get("body") != ""):
-                fieldValues = list[total].get("event").get("body").get("formSubmission").get("fieldValues") #Cycle through all 6 of the field values we acquire
-                print(fieldValues)
+    print(data)
