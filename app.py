76fdfd9543759cb33d00e9341febdec2e89bf2ba
Nickolas Rodriguez <52770090+Nick-prog@users.noreply.github.com>
2022-01-13 14:38:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,4 @@
 import Webhook
-import sys
-import ctypes
 
-try:
-    total1 = Webhook.total(method=1)
-    Webhook.uploaded_documents()
-    ctypes.windll.user32.MessageBoxW(0, "%s uploaded documents pulled." %(total1), "Complete!", 64)
-
-    total2 = Webhook.total(method=2)
-    Webhook.fee_documents()
-    ctypes.windll.user32.MessageBoxW(0, "%s fee documents pulled." %(total2), "Complete!", 64)
-except ValueError:
-    ctypes.windll.user32.MessageBoxW(0, "ValueError encountered at entry %s. %s" %(len(Webhook.IDs), sys.exc_info()[1]), "Warning!", 16)
-    pass
-except TypeError:
-    ctypes.windll.user32.MessageBoxW(0, "TypeError encountered at entry %s. %s" %(len(Webhook.IDs), sys.exc_info()[1]), "Warning!", 16)
-    pass
-except:
-    ctypes.windll.user32.MessageBoxW(0, "Something terrible occurred at entry %s. %s, %s" %(len(Webhook.IDs), sys.exc_info()[0], sys.exc_info()[1]), "Warning!", 16)
-    pass
+Webhook.uploaded_documents()
+Webhook.fee_documents()
