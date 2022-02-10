b26afd1152dbb803615f634736c44ba4ab718aac
Nickolas Rodriguez <52770090+Nick-prog@users.noreply.github.com>
2022-02-10 12:56:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 import Webhook
-import flask
-from flask import Flask, request, abort
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 @app.route('/')
 def index():
     return "<h1>Welcome!</h1>"
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/metadata', methods=['GET'])
 def get_webhook():
+    if request.method == 'GET':
+        return jsonify({
+            "MESSAGE" : "Connection confirmed.",
+            "METHOD": "GET"
+        })
+
+@app.route('/webhook', methods=['POST'])
+def post_webhook():
     if request.method == 'POST':
-       data = request.json
-       Webhook.uploaded_documents(data)
-       return data
-    else:
-        abort(400)
+        data = request.json
+        #Webhook.uploaded_documents(data)
+        print(data)
+        return jsonify({
+            "MESSAGE" : "Connection confirmed.",
+            "METHOD": "POST"
+        })
         
 if __name__ == '__main__':
     app.run(threaded=True)
